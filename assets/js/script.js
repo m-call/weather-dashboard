0041e2c22068151eb1f105a8cc0d4f6571a9d100
Michael Callahan <mcallahan0@yahoo.com>
2021-06-28 01:10:29 -0400

    Added id's for HTML elements and created an event listener in JavaScript for the search button that calls a newly created function which fetches the data for whatever value was inputted by the user, if it exists.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
-var apiKey = "bc0e6bde862987722740c53628de2aa5";
-var userInput = "";
+var submit = document.getElementById("submit");
+var city = document.getElementById("city");
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humid = document.getElementById("humid");
+var uvi = document.getElementById("uvi");
 
-fetch ("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial&appid=" + apiKey)
+var apiKey = "bc0e6bde862987722740c53628de2aa5";
+
+submit.addEventListener('click', searchWeather);
+
+function searchWeather(event) {
+
+    event.preventDefault();
+    var input = document.getElementById("input").value.trim();
+
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=" + apiKey)
     .then(function (res) {
         return res.json();
     })
     .then(function (data) {
         console.log(data);
     })
+
+}
